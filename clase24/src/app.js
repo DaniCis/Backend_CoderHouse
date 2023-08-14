88837456061a7cb7b7cea58b063d532febf1c33f
DaniCis <danicisneros2@hotmail.com>
2023-08-13 22:34:25 -0500

    practica terminada

--- clase24/src/app.js
+++ clase24/src/app.js
@@ -5,7 +5,10 @@ import viewRouter from './routes/views.router.js'
 import coursesRouter from './routes/courses.router.js'
 import usersRouter from './routes/users.router.js'
 import handlebars from 'express-handlebars'
-
+import sessionsRouter from './routes/sessions.router.js'
+import initializePassport from './config/passport.config.js';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
 import mongoose from "mongoose";
 
 const app =express();
@@ -18,10 +21,16 @@ app.engine('handlebars',handlebars.engine());
 app.set('views',__dirname+'/views')
 app.set('view engine','handlebars')
 
+initializePassport();
+app.use(passport.initialize());
+app.use(cookieParser());
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/',viewRouter)
 app.use('/api/courses',coursesRouter)
 app.use('/api/users',usersRouter)
+app.use('/api/sessions',sessionsRouter);
+
 
 const server =app.listen(PORT,()=>console.log("Server Arriba"))
--- clase24/src/routes/users.router.js
+++ clase24/src/routes/users.router.js
@@ -1,24 +1,61 @@
 import { Router } from 'express';
 import Courses from '../dao/dbManagers/courses.js';
+import passport from "passport";
+import Users from "../dao/dbManagers/users.js";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 const coursesManager = new Courses();
+const usersManager = new Users()
 
-router.get('/',async(req,res)=>{
-    let courses = await coursesManager.getAll();
-    res.send({status:"success",payload:courses})
-})
+router.get("/", async (req, res) => {
+    let users = await usersManager.getAll();
+    if (!users)
+      return res
+        .status(500)
+        .send({ status: "error", error: "No pude traer informacion" });
+  
+    res.send({ status: "success", payload: users });
+  });
+  
+  router.post("/", async (req, res) => {
+    const { first_name, last_name, email, dni, birthDate, gender } = req.body;
+    if(!first_name || !last_name || !email || !dni || !birthDate || !gender) 
+        return res.status(400).send({status:'error',error:'incomplete data'})
+    let result = await usersManager.saveUser({
+      first_name,
+      last_name,
+      email,
+      dni,
+      birthDate,
+      gender,
+    });
+    if(!result) return res.status(500).send({status:'success',payload:result})
+    res.send({ status: "success", payload: result });
+});
 
-router.post('/',async(req,res)=>{
-    const {title,description} = req.body;
-    let newCourse = {
-        title,
-        description,
-        users:[],
-        teacher:'sin asignar'
-    }
-    const result = await coursesManager.saveCourse(newCourse);
-    res.send({status:"success",payload:result});
-})
+router.post("/:uid/courses/:cid", async (req, res) => {
+    const { uid, cid } = req.params;
+    const course = await coursesManager.getById(cid);
+  
+    if (!course)
+      return res.status(404).send({ status: "error", error: "Course not found" });
+    const user = await usersManager.getById({ _id: uid });
+    if (!user)
+      return res.status(404).send({ status: "error", error: "User not found" });
+  
+    let courseExist = user.courses.some((c) => c._id.toString() === cid);
+    if (!courseExist)
+      return res
+        .status(404)
+        .send({ status: "error", error: "User not found in this course" });
+  
+    user.courses.push(course._id);
+    course.students.push(user._id);
+    await usersManager.updateUser(uid, user);
+    await coursesManager.updateCourse(cid, course);
+    res.send({ status: "success", message: "user add to course" });
+  });
+  
 
 export default router;
