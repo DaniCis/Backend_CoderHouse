50a997e8d6d2fe942a630f398a1c5054b12af2ef
DaniCis <danicisneros2@hotmail.com>
2023-09-30 18:31:05 -0500

    swagger

--- clase37/src/dao/dbManagers/courses.js
+++ clase37/src/dao/dbManagers/courses.js
@@ -4,23 +4,19 @@ export default class Courses{
    constructor(){
       console.log(" Working in mongoDb")
    }
-
    getAll =async() =>{
       let courses = await cousersModel.find().lean().populate('students');
       return courses;
    }
-
    saveCourse =async (course)=>{
       let result = await cousersModel.create(course);
       return result;
    } 
-
    updateCourse =async (id,course)=>{
       delete course._id; 
       let result = await cousersModel.updateOne({_id:id},{$set:course})
       return result;
    } 
-
    getById =async(id)=>{
       let course =await cousersModel.findOne({_id:id}.populate('students'))
       return course;
